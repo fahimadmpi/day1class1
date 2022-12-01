5a31c122f22f544cec72bbc419b551f670d93309
fahimadmpi <115918688+fahimadmpi@users.noreply.github.com>
2022-12-01 11:38:26 +0600

    Add files via upload

--- themeRitu2/functions.php
+++ themeRitu2/functions.php
@@ -6,3 +6,36 @@ wp_enqueue_style('style-bootstrap',get_template_directory_uri().'/assets/css/boo
 wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 
 add_theme_support('custom-logo');
+
+register_nav_menus([
+    'Top menu'=>'primary',
+    'BM'=>'fotter'
+]);
+
+register_sidebar([
+ 'name'=>'Banner',
+ 'id'=>'banner',
+ 'before_widget'=>'',
+ 'after_widget'=>''
+]);
+
+register_sidebar([
+ 'name'=>'Side Image',
+ 'id'=>'sideimg',
+ 'before_widget'=>'',
+ 'after_widget'=>''
+]);
+
+register_sidebar([
+ 'name'=>'Video',
+ 'id'=>'sidevideo',
+ 'before_widget'=>'',
+ 'after_widget'=>''
+]);
+
+register_sidebar([
+ 'name'=>'List',
+ 'id'=>'list',
+ 'before_widget'=>'',
+ 'after_widget'=>''
+]);
--- themeRitu2/index.php
+++ themeRitu2/index.php
@@ -68,12 +68,17 @@
                 <div class="container-fluid">
                   
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <?php wp_nav_menu(array(
+                      'menu_location'=>'Top_menu',
+                      'menu_class'=>'navbar-nav top_menu'
+
+                    )); ?>
+                    <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                       <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">হোম</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">বাংলাদেশ সম্পর্কিত</a>
+                        <a class="nav-link active" aria-current="page" href="#"><a href="#">বাংলাদেশ সম্পর্কিত</a>
                       </li>
                       <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">ই-সেবাসমূহ</a>
@@ -102,29 +107,216 @@
                       </li>
                      
                       </li>
-                    </ul>
+                    </ul> -->
                   </div>
                 </div>
               </nav>
         </div>
     </section>
     <!-- menu part end -->
-    <!-- hero part start -->
-    <section class="con">
+   <!-- hero part start -->
+   <section class="cont">
         <div class="row hero">
-            <div class="col-lg-8 hero_main ps-0">
-                <div class="banner"></div>
-                <div class="slider"></div>
-                <div class="tab"></div>
-                <div class="list"></div>
-                <div class="others"></div>
-                <?php wp_footer(); ?>
-                <a href="#"><img src="/assets/images/slider/slider/padmabanner.jpg" alt=""></a>
+            <div class="col-lg-8 hero_main">
+                <div class="banner">
+                    <!-- banner star -->
+                    <?php dynamic_sidebar('banner') ?>
+                    <img src="./image/slider/padmabanner.jpg" class="d-block w-100" alt="">
+                    <!-- banner end -->
+                </div>
+                <div class="slider">
+                    <!-- slider part -->
+                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                          <div class="carousel-item active">
+                            <img src="./image/slider/333_gov.png" class="d-block w-100" alt="...">
+                          </div>
+                          <div class="carousel-item">
+                            <img src="./image/slider/banner-renew-your-passport.png" class="d-block w-100" alt="...">
+                          </div>
+                          <div class="carousel-item">
+                            <img src="./image/slider/our_pride.png" class="d-block w-100" alt="...">
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
+                    <!-- slider end -->
+                </div>
+                <!-- tab start -->
+                <div class="tab">
+                  <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">জনপ্রিয় সেবা</button>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">নতুন সেবা</button>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">মোবাইল সেবা</button>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>দপ্তর ভিত্তিক সেবা</button>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#pills-disabled" type="button" role="tab" aria-controls="pills-disabled" aria-selected="false" disabled>সকল ই-সেবা</button>
+                      </li>
+                    </ul>
+                    <div class="tab-content" id="pills-tabContent">
+                      <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+                          <div class="row text-center">
+                              <div class="col-lg-2">
+                                  <a href="#"><img src="./image/tab/agriculture.png" alt="">
+                                      <br><p>home</p>
+                                  </a>
+              
+                              </div>
+                              <div class="col-lg-2">
+                                  <a href="#"><img src="./image/tab/agriculture.png" alt="">
+                                      <br><p>home</p>
+                                  </a>
+              
+                              </div>
+                              <div class="col-lg-2">
+                                  <a href="#"><img src="./image/tab/agriculture.png" alt="">
+                                      <br><p>home</p>
+                                  </a>
+              
+                              </div>
+                              <div class="col-lg-2">
+                                  <a href="#"><img src="./image/tab/agriculture.png" alt="">
+                                      <br><p>home</p>
+                                  </a>
+              
+                              </div>
+                              <div class="col-lg-2">
+                                  <a href="#"><img src="./image/tab/agriculture.png" alt="">
+                                      <br><p>home</p>
+                                  </a>
+              
+                              </div>
+                          </div>
+                      </div>
+                      <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">...</div>
+                      <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">...</div>
+                      <div class="tab-pane fade" id="pills-disabled" role="tabpanel" aria-labelledby="pills-disabled-tab" tabindex="0">...</div>
+                    </div>
+              </div>
+                <!-- tab end -->
+                <div class="list">
+                  <div class="list">
+                    <h5>উদ্যোগ</h5>
+                    <ul>
+                        <li><a href="">
+                          <?php dynamic_sidebar('list'); ?>
+                           <!-- বাংলাদেশ সরকারের সপ্তম পঞ্চবার্ষিক পরিকল্পনা (২০১৬-২০২০</a></li>
+                        <li><a href="">বাংলাদেশে ঘূর্ণিঝড়ের জরুরি প্রস্তুতি পরিকল্পনা।</a></li>
+                        <li><a href="">বাংলাদেশ সরকারের ষষ্ঠ পঞ্চবার্ষিক পরিকল্পনা। </a></li>
+                        <li><a href="">বাংলাদেশ সরকারের প্রেক্ষিত পরিকল্পনা (২০১০-২০২১)।</a></li>
+                        <li><a href="">দূর্যোগ ব্যবস্থাপনা জন্য জাতীয় পরিকল্পনা ২০১০-২০১৫।</a></li> -->
+                    </ul>
+                </div>
+                </div>
+                <div class="others">
+
+                </div>
+                
             </div>
-            <div class="col-lg-8 hero_side"></div>
-        </div>
+          
+            <div class="col-lg-4 hero_side">
+              <div class="side_img">
+                <a href="#">
+                  <?php dynamic_sidebar('sideimg');?>
+              <!-- <img src="./image/sidebar/Bangladesh-Directory.jpg" class="d-block w-100 pb-2" alt=""></a> -->
+
+                </a>
+               
+               
+            </div>
+            <h5>সকল বাতায়ন</h5>
+            <form action="">
+              <select name="" id="">
+                <option value="">ওয়েবসাইট বাছাই করুন</option>
+                <option value="">মন্ত্রণালয়</option>
+                <option value="">অধিদপ্তর </option>
+                <option value="">ঢাকা বিভাগ</option>
+                <option value="">চট্টগ্রাম বিভাগ</option>
+                <option value="">রাজশাহী বিভাগ</option>
+              </select>
+              <button btn>search</button>
+            </form>
+            <div class="side_video">
+                <h5>মুজিব১০০ আ্যাপ</h5>
+                <?php dynamic_sidebar('sidevideo'); ?>
+                <!-- <iframe width="315" height="200" src="https://www.youtube.com/embed/4Om3kZJL-qU" title="MUJIB100 APP | Speeches, Quotes, Books & More | Get Inspired Everyday" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+              <h5>মাস্ক পরুন সেবা নিন
+              </h5>
+                <!-- <iframe width="315" height="200" src="https://www.youtube.com/embed/4Om3kZJL-qU" title="MUJIB100 APP | Speeches, Quotes, Books & More | Get Inspired Everyday" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+
+                <!-- <iframe width="315" height="200" src="https://www.youtube.com/embed/4Om3kZJL-qU" title="MUJIB100 APP | Speeches, Quotes, Books & More | Get Inspired Everyday" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+            </div>
+            </div>
+          </div>
     </section>
     <!-- hero part end -->
+
+<!-- footer part start -->
+<footer class="cont">
+  <div class="row footer_top mt-5">
+    <img src="./image/footer/footer_top_bg.png" alt="">
+  </div>
+  <div class="row footer_bottom">
+  <div class="col-lg-7 fb_left">
+    <div class="row main-menu">
+      <nav class="navbar navbar-expand-lg bg-light">
+          <div class="container-fluid">
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">গোপনীয়তার নীতিমালা</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">ব্যবহারের শর্তাবলি</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">সার্বিক সহযোগিতায়</a>
+                </li><li class="nav-item">
+                  <a class="nav-link" href="#">সাইট ম্যাপ</a>
+                </li><li class="nav-item">
+                  <a class="nav-link" href="#">সচরাচর জিজ্ঞাসা</a>
+                
+                 
+              
+               
+               
+              </ul>
+             
+            </div>
+          </div>
+        </nav>
+        <p>সাইটটি শেষ হাল-নাগাদ করা হয়েছে: ২০২২-১০-৩০ ০৮:৩৫:০১</p>
+  </div>
+  </div>
+  <div class="col-lg-5 fb_right text-end">
+    <p>পরিকল্পনা ও বাস্তবায়নে: এটুআই, মন্ত্রিপরিষদ বিভাগ, বিসিসি, বেসিস, ডিওআইসি</p>
+    <span>  কারিগরি সহায়তায়:</span>
+    <img src="./image/footer/np-logo-set.png" alt="">
+  </div>
+  </div>
+ </footer>
+ <!-- footer part end -->
+ <!-- footer -->
+ 
+ <!-- footer -->
+
+    <script src="./js/bootstrap.bundle.min.js"></script> 
     
 </body>
 </html>
